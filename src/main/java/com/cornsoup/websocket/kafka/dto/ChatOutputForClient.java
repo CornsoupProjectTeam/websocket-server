257c0359d8dc0a69152ba1ede219f318ac274775
ujin6666 <ujin736@gmail.com>
2025-06-02 13:55:04 +0900

    Revised Kafka message schema

--- src/main/java/com/cornsoup/websocket/kafka/dto/ChatOutputForClient.java
+++ src/main/java/com/cornsoup/websocket/kafka/dto/ChatOutputForClient.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatOutputForClient {
+    private String type;
     private String message;
     private String timestamp;
 }
--- src/main/java/com/cornsoup/websocket/kafka/dto/ChatOutputWithTypeForReact.java
+++ src/main/java/com/cornsoup/websocket/kafka/dto/ChatOutputWithTypeForReact.java
@@ -7,8 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ChatOutputWithTypeForClient {
+public class ChatOutputWithTypeForReact {
     private String type;
-    private String message;
     private String timestamp;
 }
